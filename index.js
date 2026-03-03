60b51ee54663f3ddc4c0ecd008cd167eb9c03aa7
fierrenoz <fierrennozinilorenzo@gmail.com>
2026-03-03 10:48:24 +0100

    connexion à la base de données réussi

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const client = require("pg");
+const {Pool} = require("pg");
+
 server = express();
 
-server.get('/', (req, res)=>{
-    res.send("salut");
+const pool = new Pool({
+    host:"localhost",
+    user:"usr",
+    password:"usr",
+    database:"fps",
+    port:5432
+})
+
+server.get('/', async (req, res)=>{
+    const client = await pool.connect();
+    let resultat = await client.query('select * from joueur');
+    res.send(resultat);
 });
 server.listen(9090)
