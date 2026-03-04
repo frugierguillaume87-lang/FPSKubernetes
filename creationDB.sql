a6fdb23fb111f67f13a7f672d58b6b201974da57
fierrenoz <fierrennozinilorenzo@gmail.com>
2026-03-04 14:07:07 +0100

    script pour création de la db

---
+++ creationDB.sql
@@ -0,0 +1,26 @@
+
+DROP IF EXISTS users;
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    created_at DATE NOT NULL
+);
+
+DROP IF EXISTS scores;
+CREATE TABLE scores(
+    id SERIAL PRIMARY KEY,
+    user_id int NOT NULL,
+    score int not null,
+    wave_reached int not null,
+    created_at date not null,
+    Foreign Key (user_id) REFERENCES users(id)
+);
+
+DROP IF EXISTS saves;
+CREATE TABLE saves(
+    id SERIAL PRIMARY KEY,
+    user_id int NOT NULL,
+    json_data TEXT NOT NULL,
+    Foreign Key (user_id) REFERENCES users(id)
+);
