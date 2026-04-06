eacda427eb13151f9159571330d3fbf97f546576
fierrenoz <fierrennozinilorenzo@gmail.com>
2026-04-06 09:46:48 +0200

    insertion dans la base de donnée fonctionnelle et modification des endpoints afin que cela corresponde au nouvelles tables du jeu

--- dbConnect.js
+++ dbConnect.js
@@ -5,15 +5,22 @@ const db = require("pg")
  async function selectIntoDb(request, client){
 
     let resultat = await client.query(request);
-    await client.end();
+
     return resultat.rows
 }
 
 
+/*
+    fonction pour insertion et mise à jour dans la base de donnée
+    @param request pour la requête (une string),
+    @param client pour le client, qui sera release automatiquement
+    @param parameters pour les paramètres d'insertion ou de mise à jour
+
+*/
  async function insertIntoDb(request, client, parameters){
     
     await client.query(request, parameters);
-    await client.end();
+    await client.release();
 
 }
 
--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 const express = require("express");
-const {Pool} = require("pg");
-const {selectIntoDb, insertIntoDb} = require("./dbConnect")
+const { Pool } = require("pg");
+const { selectIntoDb, insertIntoDb } = require("./dbConnect");
 
 server = express();
 
 const pool = new Pool({
-    host:"localhost",
-    user:"usr",
-    password:"usr",
-    database:"fps",
-    port:5432
-})
-
-server.get('/', async (req, res)=>{
-    const client = await pool.connect();
-    let resultat = await client.query('select * from users', client);
-    await console.log(resultat.rows)
-    res.send(resultat.rows);
+  host: "localhost",
+  user: "usr",
+  password: "usr",
+  database: "fps",
+  port: 5432,
 });
-server.post("/register", async (req, res)=>{
-    const client = await pool.connect();
-    const date = new Date();
-    await insertIntoDb("INSERT INTO users(email, password, created_at) VALUES ($1, $2, $3)",client,["test","test", new Date()])
-    await client.end();
-});
-server.post("/login", async (req, res)=>{
-    const client = await pool.connect();
 
-    await client.end();
+server.get("/", async (req, res) => {
+  const client = await pool.connect();
+  let resultat = await selectIntoDb("select * from users as usr", client);
+  await console.log(resultat.rows);
+  await client.release();
+  res.send(resultat.rows);
+});
+server.post("/register", async (req, res) => {
+  const client = await pool.connect();
+  const data = req.body;
+  console.log(data)
+
+  try {
+    await insertIntoDb(
+      "INSERT INTO users(email, password, created_at) VALUES ($1, $2, $3)",
+      client,
+      ["test", "jean", new Date()],
+    );
+    client.end()
+    res.status(201)
+  } catch (error) {
+    console.log(error);
+  }
+});
+server.post("/login", async (req, res) => {
+  const client = await pool.connect();
+
+  await client.release();
 });
 server.listen(9090);
